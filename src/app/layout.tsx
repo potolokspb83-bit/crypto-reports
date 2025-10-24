647a99d0fb8adbc7e07c676dafac1e6ef26400fd
Иван Иванов <ivan@example.com>
2025-10-24 21:33:29 +0300

    Add Yandex verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ export const metadata: Metadata = {
   title: 'Crypto Reports',
   description: 'Самонаполняющийся сайт с отчётами по криптовалютам',
   verification: {
-    google: '6jqfac7yPFEms07QvwfwU9i1889NiqaSqwzjSNwous0'  // Только content (твой код от Google)
+    google: '6jqfac7yPFEms07QvwfwU9i1889NiqaSqwzjSNwous0',  // Твой Google код
+    yandex: '49baec880e26d8ff'  // Твой Yandex код
   }
 };
 
